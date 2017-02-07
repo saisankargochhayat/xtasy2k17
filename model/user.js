bca2adb5ee392181247ca0d3e42af0a6ecd34e51
Saisankar Gochhayat <saisankargochhayat@gmail.com>
2017-02-07 15:00:32 +0530

    added sample user schema

--- model/user.js
+++ model/user.js
@@ -1,2 +1,40 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
+mongoose.connect('mongodb://localhost/xtasy');
+
+var assert = require('assert');
+var bcrypt = require('bcryptjs');
+var mongoose = require('mongoose');
+SALT_WORK_FACTOR = 10;
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+
+  var userSchema = mongoose.Schema({
+    username: {type:String, required:true, unique:true},
+    password: {type:String, required:true},
+    // basearea : {type:String,required:true},
+    // conquered : Array,
+    // score : {type:Number , required:true}
+  });
+  userSchema.pre('save', function(next) {
+      var user = this;
+
+      // only hash the password if it has been modified (or is new)
+      if (!user.isModified('password')) return next();
+
+      // generate a salt
+      bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+          if (err) return next(err);
+
+          // hash the password along with our new salt
+          bcrypt.hash(user.password, salt, function(err, hash) {
+              if (err) return next(err);
+
+              // override the cleartext password with the hashed one
+              user.password = hash;
+              next();
+          });
+      });
+  });
+
+module.exports = mongoose.model('User', userSchema);
