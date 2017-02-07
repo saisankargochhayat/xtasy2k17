13a0eee0e4464e7e7ea6e9e67e7a6641156990ca
SantanuKar43 <kar.santanu43@gmail.com>
2017-02-07 16:00:32 +0530

    user greeting in ejs working

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
-
+var user = {username:'sansani'};
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', user);
 });
 
 module.exports = router;
