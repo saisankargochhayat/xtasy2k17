f3f0fa175689aeac84b509d909095e5bafceb46d
SanskritiMohapatra42 <sanskriti.mohapatra223@gmail.com>
2017-02-08 20:35:10 +0530

    testing of signing pafe

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var user = {username:'sansani'};
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', user);
+  res.render('login-register', user);
 });
 
 module.exports = router;
