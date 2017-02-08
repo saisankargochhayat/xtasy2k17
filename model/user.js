a45bf71021efa40f9006dfbc760b7eb4bcc82764
SantanuKar43 <kar.santanu43@gmail.com>
2017-02-08 22:53:17 +0530

    bcrypt fix

--- model/user.js
+++ model/user.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 mongoose.createConnection('mongodb://localhost/xtasy');
 
 var assert = require('assert');
-// var bcrypt = require('bcrypt');
+var bcrypt = require('bcrypt-nodejs');
 SALT_WORK_FACTOR = 10;
 
 var db = mongoose.connection;
@@ -22,25 +22,25 @@ db.on('error', console.error.bind(console, 'connection error:'));
     // score : {type:Number , required:true}
   });
 
-  // userSchema.pre('save', function(next) {
-  //     var user = this;
-  //
-  //     // only hash the password if it has been modified (or is new)
-  //     if (!user.isModified('password')) return next();
-  //
-  //     // generate a salt
-  //     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-  //         if (err) return next(err);
-  //
-  //         // hash the password along with our new salt
-  //         bcrypt.hash(user.password, salt, function(err, hash) {
-  //             if (err) return next(err);
-  //
-  //             // override the cleartext password with the hashed one
-  //             user.password = hash;
-  //             next();
-  //         });
-  //     });
-  // });
+  userSchema.pre('save', function(next) {
+      var user = this;
+
+      // only hash the password if it has been modified (or is new)
+      if (!user.isModified('password')) return next();
+
+      // generate a salt
+      bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+          if (err) return next(err);
+
+          // hash the password along with our new salt
+          bcrypt.hash(user.password, salt,null, function(err, hash) {
+              if (err) return next(err);
+
+              // override the cleartext password with the hashed one
+              user.password = hash;
+              next();
+          });
+      });
+  });
 
 module.exports = mongoose.model('User', userSchema);
