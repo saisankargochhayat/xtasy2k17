43205b333a710505f92b970ba8e3b3e63f7a1b82
SantanuKar43 <kar.santanu43@gmail.com>
2017-02-08 21:30:42 +0530

    testing signup/signin page

--- model/user.js
+++ model/user.js
@@ -1,8 +1,8 @@
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/xtasy');
+mongoose.createConnection('mongodb://localhost/xtasy');
 
 var assert = require('assert');
-var bcrypt = require('bcryptjs');
+// var bcrypt = require('bcrypt');
 SALT_WORK_FACTOR = 10;
 
 var db = mongoose.connection;
@@ -21,25 +21,26 @@ db.on('error', console.error.bind(console, 'connection error:'));
     // conquered : Array,
     // score : {type:Number , required:true}
   });
-  userSchema.pre('save', function(next) {
-      var user = this;
 
-      // only hash the password if it has been modified (or is new)
-      if (!user.isModified('password')) return next();
-
-      // generate a salt
-      bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-          if (err) return next(err);
-
-          // hash the password along with our new salt
-          bcrypt.hash(user.password, salt, function(err, hash) {
-              if (err) return next(err);
-
-              // override the cleartext password with the hashed one
-              user.password = hash;
-              next();
-          });
-      });
-  });
+  // userSchema.pre('save', function(next) {
+  //     var user = this;
+  //
+  //     // only hash the password if it has been modified (or is new)
+  //     if (!user.isModified('password')) return next();
+  //
+  //     // generate a salt
+  //     bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+  //         if (err) return next(err);
+  //
+  //         // hash the password along with our new salt
+  //         bcrypt.hash(user.password, salt, function(err, hash) {
+  //             if (err) return next(err);
+  //
+  //             // override the cleartext password with the hashed one
+  //             user.password = hash;
+  //             next();
+  //         });
+  //     });
+  // });
 
 module.exports = mongoose.model('User', userSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
-var user = {username:'sansani'};
+var user = require('../model/user');
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('login-register', user);
+  res.render('login-register');
+});
+
+router.get('/signup',function(req,res,next){
+  var user1 = new user({
+    name:'Hero user',
+    password: 'abcd123',
+    email: 'chiku@gmail.com',
+    college:'CET',
+    year:'4',
+    phone:'90909',
+    gender:'male'
+  }
+  );
+  user1.save(function(err){
+    if(err){
+      console.log(err);
+    }
+  })
+  res.end();
+  console.log();
 });
 
 module.exports = router;
