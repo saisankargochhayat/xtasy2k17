06e73c6059cca59edc3d592c010cebef9a31c6a2
anisha1234 <anisha.swain@yahoo.in>
2017-02-15 13:52:18 +0530

    ejs format setup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-  var express = require('express');
+var express = require('express');
 var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
   //res.render('login-register');
+});
+ 
+router.get('/home', function(req, res, next) {
+  res.render('home'); 
 });
 
 module.exports = router;
