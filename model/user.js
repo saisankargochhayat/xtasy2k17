8c5fba313fd0a4ff4a2beb95fed8573569e49bc2
Lplenka <laluprasad.lenka95@gmail.com>
2017-02-17 08:41:47 +0530

    Now xtasy id is generated :tada: :sparkles:

--- model/user.js
+++ model/user.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
-
+autoIncrement = require('mongoose-auto-increment');
 
 var assert = require('assert');
 var bcrypt = require('bcrypt-nodejs');
@@ -7,48 +7,85 @@ SALT_WORK_FACTOR = 10;
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
+autoIncrement.initialize(db);
 
-  var userSchema = mongoose.Schema({
-    name:{type:String, required:true},
-    password: {type:String, required:true},
-    email: {type:String, required:true, unique:true},
-    college:{type:String, required:true},
-    year:{type:String, required:true},
-    phone:{type:String, required:true},
-    gender:{type:String, required:true},
-    events:{type:[String]},
-    is_verified:{type:Boolean, default:false}
+var userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    college: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: String,
+        required: true
+    },
+    phone: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    events: {
+        type: [String]
+    },
+    is_verified: {
+        type: Boolean,
+        default: false
+    }
     // basearea : {type:String,required:true},
     // conquered : Array,
     // score : {type:Number , required:true}
-  });
+});
 
-  userSchema.pre('save', function(next) {
-      var user = this;
+userSchema.plugin(autoIncrement.plugin, {
+    model: 'User',
+    field: 'xtasyId',
+    startAt: 1000,
+    incrementBy: 1
+});
 
-      // only hash the password if it has been modified (or is new)
-      if (!user.isModified('password')) return next();
 
-      // generate a salt
-      bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
-          if (err) return next(err);
 
-          // hash the password along with our new salt
-          bcrypt.hash(user.password, salt,null, function(err, hash) {
-              if (err) return next(err);
+userSchema.pre('save', function(next) {
+    var user = this;
 
-              // override the cleartext password with the hashed one
-              user.password = hash;
-              next();
-          });
-      });
-  });
+    // only hash the password if it has been modified (or is new)
+    if (!user.isModified('password')) return next();
 
-  userSchema.methods.comparePassword = function(candidatePassword, cb) {
+    // generate a salt
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+
+        // hash the password along with our new salt
+        bcrypt.hash(user.password, salt, null, function(err, hash) {
+            if (err) return next(err);
+
+            // override the cleartext password with the hashed one
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword, cb) {
     bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
         if (err) return cb(err);
         cb(null, isMatch);
     });
-  };
+};
 
 module.exports = mongoose.model('User', userSchema);
