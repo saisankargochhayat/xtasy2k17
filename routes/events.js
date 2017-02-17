c052d68714b912688179501d9f4fc56478b918da
SantanuKar43 <kar.santanu43@gmail.com>
2017-02-18 00:14:10 +0530

    added more routes to events api

--- routes/events.js
+++ routes/events.js
@@ -4,7 +4,16 @@ var express = require('express'),
   Events = require('../model/events'),
   Users = require('../model/user');
 
-//event creation-only for admin page
+//get the event document -> /events/:event_id
+router.get('/:event_id',function(req,res,next){
+    Events.findOne({event_id:req.params.event_id},function(err,foundEvent){
+      if(err) return console.log(err);
+      if(!foundEvent) return res.end("event not found");
+      res.json(foundEvent);
+    });
+});
+
+//event creation-only for admin page -> /events/create/:event_id
 router.get('/create/:event_id',function(req,res,next){
   var event = new Events({
     event_id : req.params.event_id
@@ -15,7 +24,7 @@ router.get('/create/:event_id',function(req,res,next){
   });
 });
 
-//event registration
+//event registration -> /events/register/:event_id
 router.get('/register/:event_id',function(req,res,next){
   var user = req.session.user;
   console.log(user);
@@ -29,9 +38,18 @@ router.get('/register/:event_id',function(req,res,next){
   });
 });
 
-//event unregistration 
+//event unregistration -> /events/unregister/:event_id
 router.get('/unregister/:event_id',function(req,res,next){
-
+  var user = req.session.user;
+  console.log(user);
+  Events.findOneAndUpdate({event_id:req.params.event_id},{$pull:{users:user._id}},{new:true},function(err,foundevent){
+    if(err) return console.log(err);
+    if(!foundevent) return res.end("event not found");
+    Users.update({_id:mongoose.Types.ObjectId(user._id)},{$pull:{events:req.params.event_id}},function(err){
+      if(err) return console.log(err);
+    });
+    res.json(foundevent);
+  });
 });
 
 
