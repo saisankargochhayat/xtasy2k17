3451f5d23f57b319ecb7d21800cff5b047508f6f
Rishav Agarwal <rishav.159@gmail.com>
2017-02-25 19:14:37 +0530

    Fixed logout in admin panel

--- routes/admin.js
+++ routes/admin.js
@@ -40,7 +40,7 @@ router.get('/admin_signin',function(req,res,next){
   res.render('admin-signin')
 })
 router.get('/admin_signout',function(req,res,next){
-  res.session = {}
+  res.session.admin=false
   res.render('admin-signin')
 })
 router.post('/admin_signin',function(req,res,next){
